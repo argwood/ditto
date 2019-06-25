d7bca329ed9d05e0854642c49b4f0741d2b5d588
Brennen Green <brennengreen@outlook.com>
2019-06-25 19:30:56 -0400

    Backend Skeleton Build

--- ditto_backend.py
+++ ditto_backend.py
@@ -1,16 +1,31 @@
 import os
 USERDIRS = "./user_directories"
 
-def get_user_libs(user_id):
-	return "\n".join(["""children of user directory"""])
 
-def get_user_dir(user_id):
+def get_user_dir_path(user_id):
 	if str(user_id) in [name for subdir,dirs,files in os.walk(USERDIRS, topdown=False) for name in dirs]:
-		return True
+		return os.path.join(USERDIRS, str(user_id))
 	else:
 		print("Cannot find user dir.. making one")
-		create_user_dir(user_id):
-		return False
+		return create_user_dir(os.path.join(USERDIRS, str(user_id)))
 
-def create_user_dir(dir_name):
-	print("Created directory for {user_id}".format(dir_name))
+def create_user_dir(path):
+	print("Created directory {path_to_dir}".format(path_to_dir=path))
+	return path
+
+def get_user_libs(user_id):
+	user_dir_path = get_user_dir_path(user_id)
+	libs = [name for subdir, dirs, _ in os.walk(user_dir_path, topdown=False) for name in dirs]
+	return libs
+
+def create_lib(user_id, lib_name):
+	pass
+
+def add_img_to_lib(user_id, lib_name):
+	pass
+
+def get_random_image(user_id, lib_name):
+	pass
+
+def get_lib_image(user_id, lib_name, img_name):
+	pass
