6d66496ee686088a20022e556a35edea1a43f336
argwood <argwood@uchicago.edu>
2019-06-24 19:08:46 -0500

    added basic tests to recognize ditto reacts on attachments

--- ditto.py
+++ ditto.py
@@ -26,8 +26,13 @@ class Ditto:
 
 	def check_file_type(self, message):
 		''' Determine the file type of a message that has been reacted to (message.attachments)'''
+		return len(message.attachments)>0 # returns True if message includes an attachment
 
-		pass
+	def check_for_ditto_react(self, reaction):
+		ditto_emoji = discord.utils.get(reaction.message.server.emojis, name="ditto")
+		ditto_emoji=(str(ditto_emoji)[1:8])
+		print(ditto_emoji)
+		return ditto_emoji == ':ditto:'
 
 	def share_library(self):
 		''' $Library <library name> - Returns a library to message channel where left and right arrows can flip through files'''
--- start_ditto.py
+++ start_ditto.py
@@ -12,17 +12,30 @@ usage: python3 start_ditty.py
 
 client = discord.Client()
 
-token = 'NTkyNzkzOTg5NjgyNDI5OTc1.XREgqw.2EevJ7HjFfeovaDDvzB9Um4FDho'
+token = 'NTkyNzkzOTg5NjgyNDI5OTc1.XREgqw.2EevJ7HjFfeovaDDvzB9Um4FDho' # don't forget to move token to a config file
+
+_ditto = ditto.Ditto(client)
 
 @client.event
 async def on_ready():
-    print('Improving your Discord multimedia experience...')
+    print('Improving your Discord multimedia experience...') # maybe Kevin can come up with a clever message to print when the bot wakes up
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$test'):
+    if message.content.startswith('$test'): # just a basic test to be used to see if bot is on
         await client.send_message(message.channel, 'Hello!')
 
+## below should be moved to a new file; all message responses are temporary for testing
+
+@client.event
+async def on_reaction_add(reaction, user): # it's probably smarter to wait_for(ditto_react) instead of check any time someone reacts on an image - TBD but this works for now
+    message = reaction.message
+    if _ditto.check_file_type(reaction.message):
+        await client.send_message(message.channel, 'Great, that\'s a supported file type!')
+        if _ditto.check_for_ditto_react(reaction):
+            await client.send_message(message.channel, 'You even used the right reaction!') # passed the reaction tests; here you would actually call library functions
+        else: await client.send_message(message.channel, 'Okay but that\'s not a ditto')
+    else: await client.send_message(message.channel, 'You\'ve just reacted to a message...cool I guess')
 
 if __name__ == "__main__":
 
