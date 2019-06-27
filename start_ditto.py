4a19663b1ac207a3d1ba6f156ead05f9d737a6b3
Brennen Green <brennengreen@outlook.com>
2019-06-27 09:48:28 -0400

    Hiding Token

--- start_ditto.py
+++ start_ditto.py
@@ -14,7 +14,7 @@ usage: python3 start_ditto.py
 
 client = discord.Client()
 
-token = "NTkyNzkzOTg5NjgyNDI5OTc1.XREgqw.2EevJ7HjFfeovaDDvzB9Um4FDho"#os.environ['TOKEN'] # don't forget to move token to a config file
+token = os.environ['TOKEN'] # don't forget to move token to a config file
 
 _ditto = ditto.Ditto(client)
 
