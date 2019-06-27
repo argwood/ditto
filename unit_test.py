63c2073205b8986e85ba6599ed1cd774ceadf22c
Brennen Green <brennengreen@outlook.com>
2019-06-27 16:15:02 -0400

    Added random image functionality

--- unit_test.py
+++ unit_test.py
@@ -20,6 +20,7 @@ print("Current test: get_user_libs(3)")
 print(ditto_backend.get_user_libs(3))
 
 print("Testing Library Creation")
+print(ditto_backend.create_lib(3, "dogs"))
 print(ditto_backend.create_lib(4, "dogs"))
 
 print("Testing failed media retrieval")
@@ -27,6 +28,17 @@ print(ditto_backend.get_lib_image(1, "dogs", "cat"))
 
 print("Testing media download")
 print(ditto_backend.add_img_to_lib(1, "dogs", "d1", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+print(ditto_backend.add_img_to_lib(3, "dogs", "d1", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+print(ditto_backend.add_img_to_lib(3, "dogs", "d2", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+print(ditto_backend.add_img_to_lib(3, "dogs", "d3", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+print(ditto_backend.add_img_to_lib(3, "dogs", "d4", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+
+print(ditto_backend.add_img_to_lib(4, "dogs", "d1", "https://cdn.discordapp.com/attachments/592776695069671424/593794343568015382/0.jpeg"))
+
 
 print("Testing successful media retrieval")
 print(ditto_backend.get_lib_image(1, "dogs", "d1"))
+
+print("Testing random")
+print(ditto_backend.get_random_image(1, "dogs"))
+print(ditto_backend.get_random_image(3, "dogs"))
